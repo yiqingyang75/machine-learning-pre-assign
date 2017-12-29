079fb3e29c6d8b6da851a4449eff3fc4e837b702
Yiqing Yang <yiqing.yang.75@gmail.com>
2017-12-29 02:22:09 -0800

    organize structure and add test.bat

---
+++ test.bat
@@ -0,0 +1,13 @@
+:: echo off
+:: test
+py.test --nbval py/Grocery_Sales_Forecasting.ipynb
+
+:: install
+:: cd C:\Users\yiqin\myenv
+::.\Scripts\activate 
+::pip install -r requirement.txt
+
+echo Test file executed.
+echo I am too lazy to write commands again and again.
+
+pause
